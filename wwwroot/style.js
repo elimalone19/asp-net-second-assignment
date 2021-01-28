40cb47908bf117fb0eb2c70176e08a8e302b35b4
elimalone19 <elimalone19@gmail.com>
2021-01-28 01:08:53 -0700

    Made changes to js

--- wwwroot/style.js
+++ wwwroot/style.js
@@ -6,41 +6,65 @@
     let exams = $('#exams').val();
     let intex = $('#intex').val();
 
-    // calculate grade (percent)
+    // validation
+    let inputs = [];
+    inputs.push(assignments, groupProjects, quizzes, exams, intex)
+    let input_names = ['Assignments', 'Group Projects', 'Quizzes', 'Exams', 'INTEX']
+    
+    try {
+        for (let i = 0; i < inputs.length; i++) {
+            x = inputs[i]
+            if (x == "") {
+                throw "empty";
+                break
+            }
+            if (isNaN(x)) throw "not a number";
+            x = Number(x);
+            if (x < 0) throw "too low";
+            if (x > 100) throw "too high";
+        }
 
-    let result = ((Number(assignments) / 100) * 1000) + ((Number(groupProjects) / 100) * 200)
-        + ((Number(quizzes) / 100) * 200) + ((Number(exams) / 100) * 400) + ((Number(intex) / 100) * 200)
+        // calculate grade (percent)
 
-    let grade = (result / 2000) * 100
+        let result = ((Number(assignments) / 100) * 1000) + ((Number(groupProjects) / 100) * 200)
+            + ((Number(quizzes) / 100) * 200) + ((Number(exams) / 100) * 400) + ((Number(intex) / 100) * 200)
 
-    // more jquery here
-    $('#percent').text(String(grade) + '%')
+        let grade = (result / 2000) * 100
 
-    // calculate grade (letter)
+        // more jquery here
+        $('#percent').text(String(grade) + '%')
 
-    if (grade >= 93) {
-        $('#grade').text('A')
-    } else if (grade < 93 && grade >= 90) {
-        $('#grade').text('A-')
-    } else if (grade < 90 && grade >= 87) {
-        $('#grade').text('B+')
-    } else if (grade < 87 && grade >= 83) {
-        $('#grade').text('B')
-    } else if (grade < 83 && grade >= 80) {
-        $('#grade').text('B-')
-    } else if (grade < 80 && grade >= 77) {
-        $('#grade').text('C+')
-    } else if (grade < 77 && grade >= 73) {
-        $('#grade').text('C')
-    } else if (grade < 73 && grade >= 70) {
-        $('#grade').text('C-')
-    } else if (grade < 70 && grade >= 67) {
-        $('#grade').text('D+')
-    } else if (grade < 67 && grade >= 63) {
-        $('#grade').text('D')
-    } else if (grade < 63 && grade >= 60) {
-        $('#grade').text('D-')
-    } else {
-        $('#grade').text('E')
+        // calculate grade (letter)
+
+        if (grade >= 93) {
+            $('#grade').text('A')
+        } else if (grade < 93 && grade >= 90) {
+            $('#grade').text('A-')
+        } else if (grade < 90 && grade >= 87) {
+            $('#grade').text('B+')
+        } else if (grade < 87 && grade >= 83) {
+            $('#grade').text('B')
+        } else if (grade < 83 && grade >= 80) {
+            $('#grade').text('B-')
+        } else if (grade < 80 && grade >= 77) {
+            $('#grade').text('C+')
+        } else if (grade < 77 && grade >= 73) {
+            $('#grade').text('C')
+        } else if (grade < 73 && grade >= 70) {
+            $('#grade').text('C-')
+        } else if (grade < 70 && grade >= 67) {
+            $('#grade').text('D+')
+        } else if (grade < 67 && grade >= 63) {
+            $('#grade').text('D')
+        } else if (grade < 63 && grade >= 60) {
+            $('#grade').text('D-')
+        } else {
+            $('#grade').text('E')
+        }
     }
+    catch (err) {
+        alert("Your input is " + err);
+    }
+
+   
 }
